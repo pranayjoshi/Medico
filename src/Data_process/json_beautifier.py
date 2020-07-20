189380e088ad8b3026e3079b58e798daaccf4cb4
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-07-20 12:29:03 +0530

    Updated files

--- src/Data_process/json_beautifier.py
+++ src/Data_process/json_beautifier.py
@@ -1,20 +1,25 @@
 import json
-def json_to_dict(input_location):
-    with open('./data/snomed-sample-data-raw.json') as f:
-        json_object = json.load(f)
-    return json_object
-def json_beautifier(json_object):
-    converted_dict = json.dumps(json_object, indent=2)
-    return converted_dict
-def exporter(converted_dict, output_location):
-    with open(output_location, "w") as outfile: 
-        outfile.write(converted_dict) 
-def run():
-    input_location = './data/snomed-sample-data-raw.json'
-    output_location = './data/snomed_data.json'
-    json_object = json_to_dict(input_location)
-    converted_dict = json_beautifier(json_object)
-    exporter(converted_dict, output_location)
+import file_path as f_p
+class beautify:
+    def __init__(self, input_location, output_location):
+        self.input_location = input_location
+        self.output_location = output_location
+    def json_to_dict(self):
+        with open(self.input_location) as f:
+            json_object = json.load(f)
+        self.json_object = json_object
+    def json_beautifier(self):
+        converted_dict = json.dumps(self.json_object, indent=2)
+        self.converted_dict = converted_dict
+    def exporter(self):
+        with open(self.output_location, "w") as outfile: 
+            outfile.write(self.converted_dict) 
+def run_all(input_location, output_location):
+    beautify_class = beautify(input_location, output_location)
+    beautify_class.json_to_dict()
+    beautify_class.json_beautifier()
+    beautify_class.exporter()
+
 if __name__ == "__main__":
     run()
 
--- src/Speech_process/speech to text.py
+++ src/Speech_process/speech to text.py
@@ -1,17 +1,17 @@
 import speech_recognition as sr     #Recognition Module
 import pyttsx3                      #Speaking package
+import json
+import series_counter as s_c
 
 engine = pyttsx3.init()             #initialising pyttsx value
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-#testing
 speak('hi user')
 
-
-def Voice_recognize(api_key, wait_time=5):
-    #listens for commands
+def Voice_recognize(wait_time, noice_duration):
+    #listens to commands
 
     r = sr.Recognizer()             #assigning recognize value
 
@@ -19,29 +19,94 @@ def Voice_recognize(api_key, wait_time=5):
 
         speak('i am ready for your command')
         r.pause_threshold = wait_time                  # Check when the user takes the pause for 5 second
-        r.adjust_for_ambient_noise(source, duration=1)
+        r.adjust_for_ambient_noise(source, duration=noice_duration)
         audio = r.listen(source)
 
     try:
-        command = r.recognize_google(audio).lower()         #Using google's api
-        speak('you said:' + command)
+        #Using google's api, 
+        output = r.recognize_google(audio).lower()  #Well I want to use the default language so I am leaving the language field
+        speak('you said:' + output)        # use this before using "language" attribute <lang = langs["Hindi"]>
 
     
     except sr.UnknownValueError:            #loop back to continue to listen for commands if unrecognizable speech is received
         speak("Your last command couldn\'t be heard")
-        command = Voice_recognize();        # runs the code again
+        output = Voice_recognize();        # runs the code again
     
-    return command
+    return output
+class speech_to_text:
+    def __init__(self,count, output, results_loc):
+        self.count = count
+        self.output = output
+        self.results_loc = results_loc
 
-def result(command):
-    result = ">> " + command + "." + " \n"
-    expected = "so after reading your report i came to know that you have been suffering from diarrhoea and here are some medicine for you crocin 650 paracetamol and Acetaminophen"
-    print(f"Actual: {result}\nExpected: {expected}")
-    mfopener = open("./Data_store/results.txt", "a+")
-    mfopener.write(result)
-    mfopener.close()
+    def counter(self):
+        self.present_count = self.count
 
+    def result_formatter(self):
+        self.final_result = self.present_count + " "
+        self.final_result += self.output + " \n"
+
+    def result_to_txt(self):
+        with open(self.results_loc, "a+",  encoding='utf8') as mfopener:
+            mfopener.write(self.final_result)
+
+    def result_returner(self):
+        return self.final_result
+
+# this class will act as a test printer
+class test_printer:
+    def __init__(self, actual):
+        self.actual = actual
+    def test_input(self):
+        self.expected = "so after reading your report i came to know that you have been suffering from diarrhoea and here are some medicine for you crocin 650 paracetamol and Acetaminophen"
+    def printer(self):
+        out = (f"Actual: {self.actual}\nExpected: {self.expected}")
+        return out   
+class run_utils:
+# returns the path required
+    def __init__(self, present_count):
+        self.present_count = present_count
+    def file_paths(self):
+        with open("./storage/path.json", "r+") as file:       #for mailing system
+            data = json.load(file)
+        results_loc = data["results"]
+        self.results_loc = results_loc
+
+    #runs the Voice_recognize function
+    def run_Vr(self):
+        wait_time,duration=5,0.5
+        Voice_recog = Voice_recognize(wait_time, duration)
+        self.output = Voice_recog
+
+    # run all the functions in the speech_to_text class
+    def run_all_s2t(self):
+        s2t = speech_to_text(self.present_count, self.output, self.results_loc)
+        s2t.counter()
+        s2t.result_formatter()
+        s2t.result_to_txt()             
+        self.return_result = s2t.result_returner()
+
+    def run_printer(self):
+        tst_print = test_printer(self.output)
+        tst_print.test_input()
+        self.printer = tst_print.printer
+    
+    def printer(self):
+        return self.printer
+#this script will run all
+def run_all(present_count):
+    r_utils = run_utils(present_count)
+    r_utils.file_paths()
+    r_utils.run_Vr()
+    r_utils.run_all_s2t()
+    r_utils.run_printer()
+    printer = r_utils.printer()
+    return printer
+# finally run the script
 def run():
-    api_key="GOOGLE_SPEECH_RECOGNITION_API_KEY"
-    result(Voice_recognize(api_key=api_key))
+    present_count = s_c.run()
+    printer = run_all(present_count)
+    print(printer)
+if __name__ == "__main__":
+    run()
     
