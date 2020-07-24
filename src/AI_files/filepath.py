3a8e91ef8351721158786a7ff068002cdcb351a9
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-07-24 20:18:37 +0530

    added

---
+++ src/AI_files/filepath.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Data_process import file_path as f_p
+
+def run(file_name):
+    filepath = f_p.run(file_name)
+    return filepath
