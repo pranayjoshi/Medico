19b054d038bd92ed2b8391814810031321a61bce
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-07-24 22:04:20 +0530

    added

---
+++ src/AI_files/get_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json
+import filepath as fp
+def read_csv(csv_loc):
+    readcsv = pd.read_csv(csv_loc)
+    return readcsv
+def dataframe(csv):
+    data_frame = pd.DataFrame(csv)
+    return data_frame
+def get_file_path():
+    data_loc = fp.run("data")
+    return data_loc
+def run():
+    csv_loc = get_file_path()
+    csv = read_csv(csv_loc)
+    return dataframe(csv)
