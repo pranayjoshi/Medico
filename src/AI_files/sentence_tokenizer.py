db772d25ab58b5335f0dab91f152c5a11fc44b89
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-08-02 00:32:22 +0530

    Your commit message

---
+++ src/AI_files/sentence_tokenizer.py
@@ -0,0 +1,31 @@
+from spacy.lang.en import English
+import filepath as fp
+
+def get_file_path():
+    results = fp.run("results")
+    return results
+def fetch_recent_conversation(results):
+    
+class sentence_tokenizer:
+    def __init__(self):
+        pass
+    def sentence_tokenizer(recent_conversation):
+        nlp = English()
+
+        # Create the pipeline 'sentencizer' component
+        sbd = nlp.create_pipe('sentencizer')
+
+        # Add the component to the pipeline
+        nlp.add_pipe(sbd)
+
+        text = """When learning data science, you shouldn't get discouraged!
+        Challenges and setbacks aren't failures, they're just part of the journey. You've got this!"""
+
+        #  "nlp" Object is used to create documents with linguistic annotations.
+        doc = nlp(text)
+
+        # create list of sentence tokens
+        sents_list = []
+        for sent in doc.sents:
+            sents_list.append(sent.text)
+        print(sents_list)
