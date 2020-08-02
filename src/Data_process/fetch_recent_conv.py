2a87a473c2e8334ec69e99ada64e9a9f66247d72
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-08-03 01:38:58 +0530

    initial commit

---
+++ src/Data_process/fetch_recent_conv.py
@@ -0,0 +1,23 @@
+class Counter:
+    def __init__(self, results_loc):
+        self.loc = results_loc
+    # this will get the last line
+    def last_line(self):
+        with open(self.loc, 'r') as f:
+            lines = f.read().splitlines()
+            self.last_line = lines[-1]
+
+    # Extracting first word from the line
+    def extract_first_word(self):
+        last_line = self.last_line
+        self.first_word = last_line.split()[0]
+    def delete_first_word(self):
+        fst_word = self.first_word
+        del_first_word = self.last_line.strip(fst_word+" ")
+        final_recent_conv = del_first_word
+        return final_recent_conv
+def run(results_loc):
+    count = Counter(results_loc)
+    count.last_line()
+    count.extract_first_word()
+    return count.delete_first_word()
