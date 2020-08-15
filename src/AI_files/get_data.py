76af12efcf40b46256a0ccfb98c89933f08f3368
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-08-15 22:24:11 +0530

    updated

--- src/AI_files/get_data.py
+++ src/AI_files/get_data.py
@@ -8,12 +8,12 @@ def dataframe(csv):
     data_frame = pd.DataFrame(csv)
     return data_frame
 def get_final_file_path():
-    data_loc = fp.run("initial_data")
+    data_loc = fp.run("main_data")
     return data_loc
 def get_initial_file_path():
     data_loc = fp.run("initial_data")
     return data_loc
-def run(data_loc):
+def csv_to_df(data_loc):
     csv_loc = data_loc
     csv = read_csv(csv_loc)
     return dataframe(csv)
