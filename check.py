7d6dc4dd5691f4ec989479a6fde30f197453515c
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-08-18 10:20:33 +0530

    updated

--- check.py
+++ check.py
@@ -1,11 +1,11 @@
 import os
-directory = "./src"
-
+directory = "./"
+import sys
 for root, dirs, files in os.walk(directory):
     for file in files:
         if file.endswith('.py'):
             file_ptr = open(file, 'r')
             old_content = file_ptr.read()
             file_ptr = open(file, 'w')
-            file_ptr.write("hello")
+            file_ptr.write("''' \nMIT License\n\nCopyright (c) 2020 Pranay Joshi\n'''")
             file_ptr.write(old_content)
