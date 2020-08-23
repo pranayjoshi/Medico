e868c00fd402d32e6344dc02d60956ff7d0f412d
Pranay Joshi <pranayjoshi4466@gmail.com>
2020-08-23 12:18:58 +0530

    modified

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,16 +3,21 @@ import gensim
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import nltk
 from nltk.tokenize import word_tokenize
+
 def load_model():
     model= Doc2Vec.load("./storage/doc2vec.model")
     ## Print model vocabulary
     model.wv.vocab
     return model
+
 def test(model):
-    test_doc = word_tokenize("I can understand he is suffering from Chronic tophaceous gout of hand due to renal impairment".lower())
+    test_doc = word_tokenize("so after reading everything i came to know that chronic tophaceous gout obtained due to renel impairment".lower())
     model.docvecs.most_similar(positive=[model.infer_vector(test_doc)],topn=5)
     best_result = model.docvecs.most_similar(positive=[model.infer_vector(test_doc)],topn=1)
-    print("Best Result: ",best_result[0][0])
+    expected_result = "303921000119109"
+    if best_result[0][0] == expected_result:
+        print("Test passed")
+    print("Best Result: " + best_result[0][0] + "    --->    " + "Expected Result: " + "303921000119109")
     print("Sureity percntage: ",best_result[0][1])
 
 model = load_model()
